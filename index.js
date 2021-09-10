0327ca45f29ef6f867499bd19d0fd83b570a2667
mordojp <ghtnetsgrb@mail.ru>
2021-09-10 16:48:37 +0300

    started creating a password recovery form

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ const coursesRoutes = require('./routes/courses')
 const authRoutes = require('./routes/auth')
 const varMiddleware = require('./middleware/variables')
 const userMiddleware = require('./middleware/user')
+const keys = require('./keys')
 
-const MONGODB_URI = `mongodb+srv://Nikita:PZfq75cbLcVaVcs@cluster0.enryq.mongodb.net/shop`
+const PORT = process.env.PORT || 3000
+
 const app = express()
 const hbs = exphbs.create({
   defaultLayout: 'main',
@@ -23,7 +25,7 @@ const hbs = exphbs.create({
 })
 const store = new MongoStore({
   collection: 'sessions',
-  uri: MONGODB_URI
+  uri: keys.MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -33,7 +35,7 @@ app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
-  secret: 'some secret value',
+  secret: keys.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store
@@ -50,11 +52,9 @@ app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
 app.use('/auth', authRoutes)
 
-const PORT = process.env.PORT || 3000
-
 async function start() {
   try {
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(keys.MONGODB_URI, {
       useNewUrlParser: true,
       useFindAndModify: false
     })
---
+++ keys/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGODB_URI: `mongodb+srv://Nikita:PZfq75cbLcVaVcs@cluster0.enryq.mongodb.net/shop`,
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'jsmododev@gmail.com',
+    BASE_URL: 'http://localhost:3000'
+}
