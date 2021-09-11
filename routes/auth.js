c5b0f236dd66cfb0e60926310eb0a55dd68bf6b0
mordojp <ghtnetsgrb@mail.ru>
2021-09-11 21:25:36 +0300

    added validation for registration and login page

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const {Router} = require('express')
 const bcrypt = require('bcryptjs')
 const crypto = require('crypto')
+const {validationResult} = require('express-validator/check')
 const nodemailer = require('nodemailer')
 const sendgrid = require('nodemailer-sendgrid-transport')
 const User = require('../models/user')
 const keys = require('../keys')
 const regEmail = require('../emails/registration')
 const resetEmail = require('../emails/reset')
+const {registerValidators, loginValidators} = require('../utils/validators')
 const router = Router()
 
 const transporter = nodemailer.createTransport(sendgrid({
@@ -29,29 +31,30 @@ router.get('/logout', async (req, res) => {
     })
 })
 
-router.post('/login', async (req, res) => {
+router.post('/login', loginValidators, async (req, res) => {
     try {
         const {email, password} = req.body
         const candidate = await User.findOne({ email })
 
-        if (candidate) {
-            const areSame = await bcrypt.compare(password, candidate.password)
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            req.flash('loginError', errors.array()[0].msg)
+            return res.status(422).redirect('/auth/login#login')
+        }
 
-            if (areSame) {
-                req.session.user = candidate
-                req.session.isAuthenticated = true
-                req.session.save(err => {
-                    if (err) {
-                        throw err
-                    }
-                    res.redirect('/')
-                })
-            } else {
-                req.flash('loginError', 'Неверный пароль')
-                res.redirect('/auth/login#login')
-            }
+        const areSame = await bcrypt.compare(password, candidate.password)
+
+        if (areSame) {
+            req.session.user = candidate
+            req.session.isAuthenticated = true
+            req.session.save(err => {
+                if (err) {
+                    throw err
+                }
+                res.redirect('/')
+            })
         } else {
-            req.flash('loginError', 'Такого пользователя не существует')
+            req.flash('loginError', 'Неверный пароль')
             res.redirect('/auth/login#login')
         }
     } catch (e) {
@@ -59,23 +62,23 @@ router.post('/login', async (req, res) => {
     }
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', registerValidators, async (req, res) => {
     try {
-        const {email, password, repeat, name} = req.body
-        const candidate = await User.findOne({ email })
+        const {email, password, name} = req.body
 
-        if (candidate) {
-            req.flash('registerError', 'Пользователь с таким email уже существует, пожалуйста используйте другой email')
-            res.redirect('/auth/login#register')
-        } else {
-            const hashPassword = await bcrypt.hash(password, 10)
-            const user = new User ({
-                email, name, password: hashPassword, cart: {items: []}
-            })
-            await user.save()
-            res.redirect('/auth/login#login')
-            await transporter.sendMail(regEmail(email))
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            req.flash('registerError', errors.array()[0].msg)
+            return res.status(422).redirect('/auth/login#register')
         }
+
+        const hashPassword = await bcrypt.hash(password, 10)
+        const user = new User ({
+            email, name, password: hashPassword, cart: {items: []}
+        })
+        await user.save()
+        res.redirect('/auth/login#login')
+        await transporter.sendMail(regEmail(email))
     } catch (e) {
         console.log(e)
     }
