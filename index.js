cc2167cd0fc3a37f6d0ddbaae069aa17ccff0fb8
mordojp <ghtnetsgrb@mail.ru>
2021-09-12 17:12:32 +0300

    finished a profile page and added fileadding functional

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ const coursesRoutes = require('./routes/courses')
 const authRoutes = require('./routes/auth')
 const profileRoutes = require('./routes/profile')
 const varMiddleware = require('./middleware/variables')
-const errorHandler = require('./middleware/error')
 const userMiddleware = require('./middleware/user')
+const errorHandler = require('./middleware/error')
+const fileMiddleware = require('./middleware/file')
 const keys = require('./keys')
 
 const PORT = process.env.PORT || 3000
@@ -36,6 +37,7 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
   secret: keys.SESSION_SECRET,
@@ -43,6 +45,7 @@ app.use(session({
   saveUninitialized: false,
   store
 }))
+app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash())
 app.use(varMiddleware)
